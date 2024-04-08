4abb2318e167e42b993346e55101e7245da3e07a
dotnetbroo <jasurbekabdunazarov04@gmail.com>
2024-04-08 11:27:40 +0500

    Some entites added

---
+++ SmartMarket.Domin/Entities/Cards/Card.cs
@@ -0,0 +1,21 @@
+﻿using SmartMarket.Domin.Commons;
+using SmartMarket.Domin.Entities.Categories;
+using SmartMarket.Domin.Entities.Users;
+
+namespace SmartMarket.Domin.Entities.Cards;
+
+public class Card : Auditable
+{
+    public string TransNo { get; set; }
+    public string ProductName { get; set; }
+    public string BarCode { get; set; }
+    public long CategoryId { get; set; }
+    public Category Category { get; set; }
+    public decimal Price { get; set; }
+    public short DiscountPrice { get; set; }
+    public decimal Quantity { get; set; }
+    public decimal TotalPrice { get; set; }
+    public long CasherId { get; set; }
+    public User User { get; set; }
+    public string Status { get; set; }
+}
