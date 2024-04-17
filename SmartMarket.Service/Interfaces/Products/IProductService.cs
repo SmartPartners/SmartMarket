8cf5ee0e475cd79057a5c0ddaebe4456bb23a38a
dotnetbroo <jasurbekabdunazarov04@gmail.com>
2024-04-17 16:39:10 +0500

    Product Service method added:

---
+++ SmartMarket.Service/Interfaces/Products/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using SmartMarket.Domin.Configurations;
+using SmartMarket.Service.DTOs.Products;
+
+namespace SmartMarket.Service.Interfaces.Products;
+
+public interface IProductService
+{
+    Task<bool> DeleteAsync(long id);
+    Task<ProductForResultDto> GetByIdAsync(long id);
+    Task<IEnumerable<ProductForResultDto>> GetAllAsync(PaginationParams @params);
+    Task<ProductForResultDto> CreateAsync(ProductForCreationDto productForCreationDto);
+    Task<ProductForResultDto> UpdateAsync(long id, ProductForUpdateDto productForUpdateDto);
+}
---
+++ SmartMarket.Service/Services/Products/ProductService.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SmartMarket.Data.IRepositories;
+using SmartMarket.Domin.Configurations;
+using SmartMarket.Domin.Entities.Products;
+using SmartMarket.Service.Commons.Exceptions;
+using SmartMarket.Service.Commons.Extensions;
+using SmartMarket.Service.DTOs.Products;
+using SmartMarket.Service.Interfaces.Products;
+
+namespace SmartMarket.Service.Services.Products;
+
+public class ProductService : IProductService
+{
+    private readonly IRepository<Product> _productRepository;
+    private readonly IMapper _mapper;
+
+    public ProductService(IRepository<Product> productsRepository, IMapper mapper)
+    {
+        _productRepository = productsRepository;
+        _mapper = mapper;
+    }
+
+    public Task<ProductForResultDto> CreateAsync(ProductForCreationDto productForCreationDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteAsync(long id)
+    {
+        var product = await _productRepository.SelectAll()
+            .Where(p => p.Id == id)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+
+        if (product is null)
+            throw new CustomException(404, "Mahsulot topilmadi.");
+
+        return await _productRepository.DeleteAsync(id);
+    }
+
+    public async Task<IEnumerable<ProductForResultDto>> GetAllAsync(PaginationParams @params)
+    {
+        var products = await _productRepository.SelectAll()
+            .AsNoTracking()
+            .ToPagedList(@params)
+            .ToListAsync();
+
+        return _mapper.Map<IEnumerable<ProductForResultDto>>(products);
+    }
+
+    public async Task<ProductForResultDto> GetByIdAsync(long id)
+    {
+        var product = await _productRepository.SelectAll()
+             .Where(p => p.Id == id)
+             .AsNoTracking()
+             .FirstOrDefaultAsync();
+
+        if (product is null)
+            throw new CustomException(404, "Mahsulot topilmadi.");
+
+        return _mapper.Map<ProductForResultDto>(product);
+    }
+
+    public Task<ProductForResultDto> UpdateAsync(long id, ProductForUpdateDto productForUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+}
