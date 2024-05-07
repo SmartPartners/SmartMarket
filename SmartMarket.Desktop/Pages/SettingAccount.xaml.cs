52d69dee6a720a265bdb4f673f935d32a2e4ff38
Abdurahmon Jumanboyev <abdurahmonnt2003@gmail.com>
2024-05-07 17:59:38 +0500

    Created Setting AccountPage

---
+++ SmartMarket.Desktop/Pages/SettingAccount.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartMarket.Desktop.Pages
+{
+    /// <summary>
+    /// Interaction logic for SettingAccount.xaml
+    /// </summary>
+    public partial class SettingAccount : Page
+    {
+        public SettingAccount()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public async Task RefreshAsync()
+        {
+
+        }
+    }
+}
