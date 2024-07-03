418c75a9a570c7653e4d3c0ac8e020daf717ea68
Abdurahmon Jumanboyev <abdurahmonnt2003@gmail.com>
2024-07-03 17:20:03 +0500

    Hontr Agent Window Created

---
+++ SmartMarket.Desktop/Windows/ContrAgents/ContrAgentsUpdateWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SmartMarket.Desktop.Windows.ContrAgents
+{
+    /// <summary>
+    /// Interaction logic for ContrAgentsUpdateWindow.xaml
+    /// </summary>
+    public partial class ContrAgentsUpdateWindow : Window
+    {
+        public ContrAgentsUpdateWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
