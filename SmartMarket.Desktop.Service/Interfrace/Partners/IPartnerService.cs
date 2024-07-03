87f2d78bfda44508363b683efcf0ff10618f2c28
KhaydarovAbbos <iabbos.kh@gmail.com>
2024-07-03 10:06:23 +0500

    partnerService added

---
+++ SmartMarket.Desktop.Service/Interfrace/Partners/IPartnerService.cs
@@ -0,0 +1,23 @@
+﻿using SmartMarket.Domin.Configurations;
+using SmartMarket.Service.DTOs.Partners;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMarket.Desktop.Service.Interfrace.Partners
+{
+    public interface IPartnerService
+    {
+        public Task<PartnerForResultDto> CreateAsync(PartnerForCreationDto entity);
+
+        public Task<PartnerForResultDto> ModifyAsync(long id, PartnerForUpdateDto dto);
+
+        public Task<bool> DeleteAsync(long id);
+
+        public Task<IEnumerable<PartnerForResultDto>> GetAllAsync();
+
+        public Task<PartnerForResultDto> GetByIdAsync(long id);
+    }
+}
