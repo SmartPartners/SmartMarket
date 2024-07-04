57552fc3631578db66e3c2659263d45e208d68c2
KhaydarovAbbos <iabbos.kh@gmail.com>
2024-07-04 08:59:50 +0500

    update

---
+++ SmartMarket.Desktop.Service/Service/Korzinkas/KorzinkaService.cs
@@ -0,0 +1,13 @@
+﻿using SmartMarket.Desktop.Service.Interfrace.Korzinkas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMarket.Desktop.Service.Service.Korzinkas
+{
+    public class KorzinkaService : IKorzinkaService
+    {
+    }
+}
