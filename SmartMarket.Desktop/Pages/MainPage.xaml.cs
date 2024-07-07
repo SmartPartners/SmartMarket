600eaf0af3a98d321549fa2c60af3bee69ee4881
jumanboyev <abdurahmonnt2003@gmail.com>
2024-07-07 15:01:17 +0500

    Fixed

--- SmartMarket.Desktop/Pages/MainPage.xaml.cs
+++ SmartMarket.Desktop/Pages/MainPage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SmartMarket.Desktop.Pages
 {
@@ -23,6 +11,11 @@ namespace SmartMarket.Desktop.Pages
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
